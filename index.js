1b29091a4b26cf33357af4777e545df23d9c7ae1
john.so <john.so@koreapolyschool.com>
2019-01-17 10:53:56 +0900

    1

--- index.js
+++ index.js
@@ -57,6 +57,42 @@ restService.post("/echo", function(req, res) {
       }
     ],
     source: "example.com",
+    payload: {
+      google: {
+        expectUserResponse: true,
+        richResponse: {
+          items: [
+            {
+              simpleResponse: {
+                textToSpeech: "this is a simple response"
+              }
+            }
+          ]
+        }
+      },
+      facebook: {
+        text: "Hello, Facebook!"
+      },
+      slack: {
+        text: "This is a text response for Slack."
+      }
+    },
+    outputContexts: [
+      {
+        name: "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+        lifespanCount: 5,
+        parameters: {
+          param: "param value"
+        }
+      }
+    ],
+    followupEventInput: {
+      name: "event name",
+      languageCode: "en-US",
+      parameters: {
+        param: "param value"
+      }
+    }
   });
   
 });
